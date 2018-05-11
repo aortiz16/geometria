7cd3b418194468fd83dabd2b8707f0f37f97f594
aortiz16@gmail.com <javier16>
2018-05-11 20:56:15 +0200

    Método en una línea, en la clase TrIanGulO

--- src/Triangulo_AJOB.java
+++ src/Triangulo_AJOB.java
@@ -1,29 +1,25 @@
 
-public class Triangulo extends FiguraGeometrica {
-	private double a;
-	private double b;
-	private double c;
+public class Triangulo_AJOB extends FiguraGeometrica_AJOB {
+	private double Lado1;
+	private double Lado2;
+	private double Lado3;
 	
-	public Triangulo(String tipoFigura, double lado1, double lado2, double lado3) {
+	public Triangulo_AJOB(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
-		a = lado1;
-		b = lado2;
-		c = lado3;
+		Lado1 = lado1;
+		Lado2 = lado2;
+		Lado3 = lado3;
 	}
 
 	@Override
 	public double perimetro() {
-		return a + b + c;
+		return Lado1 + Lado2 + Lado3;
 	}
 
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
-		sp = this.semiPerimetro();
-		return Math.sqrt(sp * (sp-a) * (sp-b) * (sp-c));
-	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
+		sp = this.perimetro() / 2;
+		return Math.sqrt(sp * (sp-Lado1) * (sp-Lado2) * (sp-Lado3));
 	}
 }
